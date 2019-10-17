1cff2ef413cd1676d5e5810b42924fe5ba6033d8
cdsalty <soltiscd@gmail.com>
2019-10-16 23:44:00 -0400

    the dirty work complete, finish and style

--- toDoApp.js
+++ toDoApp.js
@@ -16,7 +16,7 @@ const addTodo = todo =>
     convertObjectToString([...getTodos(), todo]) // use of spread operator
   );
 
-// Delete Todo:
+// Delete Todo: ANYTIME a delete todo is called, it will be followed by a refresh displayTodos
 const deleteTodo = todo =>
   localStorage.setItem(
     storageKey,
@@ -35,12 +35,11 @@ const appendLiToDom = el =>
   document.getElementById("todo-list-container").appendChild(el); // get the list, add the child to it
 
 // clear-out/delete the list item
-const clearTodoListDisplay = (document.getElementById(
-  "todo-list-container"
-).innerHTML = "");
-
+const clearTodoListDisplay = () => {
+  document.getElementById("todo-list-container").innerHTML = "";
+};
 // after text submitted, clear input form
-const clearInput = () => document.getElementById("new-todo-input").value;
+const clearInput = () => (document.getElementById("new-todo-input").value = "");
 
 // * BUILDIING THE TODO AND UI
 const displayTodos = () => {
@@ -52,19 +51,44 @@ const displayTodos = () => {
 
 // create and initialize all of our event listeners once our each of our todos has been loaded
 const initClickListensers = () => {
-  Array.from(document.getElementsByClassName("list-group-item")).forEach(
-    _item => {
+  Array.from(document.getElementsByClassName("list-group-item")) // get a query, convert to array type in order to run array functions
+    .forEach(_item => {
       _item.addEventListener("click", $event => {
-        const todo = $event.target.innerText;
+        const todo = $event.target.innerText; // which event the user clicked on
         if (window.confirm("Task Complete: " + todo)) {
+          // 2 step confirmation process before deleting
           // ok = true, cancel = false
           // if they do click "ok," take the appropriate steps:
           deleteTodo(todo);
           displayTodos();
         }
       });
-    }
-  );
+    });
 };
+// when the dom loads, display todos
+// REVIEW
+document.addEventListener("DOMContentLoaded", () => displayTodos()); // when the dom loads, display todos
 
-// Create an event to reset our local storage
+// TO ADD TODOS
+document
+  .getElementById("submit-new-todo-btn")
+  .addEventListener("click", $event => {
+    const newTodoInput = document.getElementById("new-todo-input");
+    if (newTodoInput.value) {
+      // SIMPLE: IF IT HAS A VALUE THEN PROCEED
+      addTodo(newTodoInput.value.trim()); //remove white-space on the ends
+      displayTodos();
+    }
+  });
+
+/* Create an event to reset our local storage
+  - create a function that listens for clicks on 'reset local storage'
+  - reset local local storage
+*/
+document
+  .getElementById("reset-storage-btn")
+  .addEventListener("click", $event => {
+    localStorage.removeItem(storageKey);
+    displayTodos(); // once we clear, we want to then display the list
+    clearInput();
+  });
